174152de372efd99defa558011ad8c67e397f4c0
MonicaBobby <30898842+MonicaBobby@users.noreply.github.com>
2017-08-20 11:07:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,14 @@
 console.log('Loaded!');
-//var element=document.getElementById=
+var display=document.getElementById("btn");
+var number=document.getElementById("txt");
+
+display.onclick(function(){
+    var start=1;
+    var result;
+    number=Number(number);
+    for(var i=0;i<number;i++){
+        result=start;
+        result=result+"<br/>";
+        div.innerHTML(result);
+    }
+});
